a81a2bc059a4b374117bf4e76807063527965adc
Easwaran <easwaranpottiak@gmail.com>
2023-12-17 23:18:00 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import './header.js'
 import Header from './header.js';
+import Body from './body.js';
 
 function App() {
   return (
-    <Header/>
+    <Header/>,
+    <Body/>
   );
 }
 
---
+++ src/body.js
@@ -0,0 +1,70 @@
+
+function Body(){
+    return (
+        <>
+  <img src="./1.png" />
+  <div
+    style={{
+      textAlign: "center",
+      backgroundColor: "#991f48",
+      color: "white",
+      paddingTop: 20
+    }}
+  >
+    <h1>Think Health. Think Massage.</h1>
+    <p>
+      We are open 9am to 6pm; Monday through Sunday. If you would like to
+      schedule an appointment
+    </p>
+    <p>with us, please call us at 987-654-3210 today!</p>
+    <div
+      style={{ display: "flex", justifyContent: "center", paddingBottom: 30 }}
+    >
+      <div style={{ border: "1px solid white", padding: 10 }}>
+        LEARN MORE ABOUT US
+      </div>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <div style={{ border: "1px solid white", padding: 10 }}>
+        CONTACT US TODAY
+      </div>
+    </div>
+  </div>
+  <p style={{ marginTop: 60, color: "#991f48" }}>
+    Are you looking for a professional, registered massage therapist? Sample
+    Massage Therapy has 4 registered massage therapists who can provide clinical
+    massage.
+  </p>
+  <p>
+    We are committed to your long term health and well-being. Our
+    multi-disciplinary clinic provides a balanced approach to a healthy
+    lifestyle. Enhance your health and improve your performance with our
+    treatments.
+  </p>
+  <p>
+    We welcome you to come explore all the benefits you’ll enjoy as one of our
+    valued guests. Our professional staff is committed to offering the best
+    massage therapy in Mainland.
+  </p>
+  <br />
+  <hr />
+  <p
+    style={{
+      marginTop: 50,
+      marginBottom: 50,
+      textAlign: "center",
+      color: "#991f48",
+      fontSize: "x-large"
+    }}
+  >
+    Family Wellness Massage Therapy
+  </p>
+  <p style={{ textAlign: "center" }}>888 Griffiths Way, Mainland ML 12345</p>
+  <p style={{ textAlign: "center" }}>Tel: 987.654.3210</p>
+  <p style={{ textAlign: "center" }}>Email: info@yoursite.com</p>
+  <br />
+</>
+
+    );
+}
+
+export default Body;
